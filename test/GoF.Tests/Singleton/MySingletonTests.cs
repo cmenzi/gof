b8a4cff7269445f075411e4e3dec64d64b265240
Cédric Menzi <cedric.menzi@buhlergroup.com>
2020-11-09 20:21:52 +0100

    MySingleton example added

---
+++ test/GoF.Tests/Singleton/MySingletonTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using GoF.Singleton;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoF.Tests.Singleton
+{
+    [TestFixture]
+    public class MySingletonTests
+    {
+        [Test]
+        public void MySingleton_When2TimesCalled_OnlyOneInstanceExists()
+        {
+            var s1 = MySingleton.GetInstance();
+            var s2 = MySingleton.GetInstance();
+            
+            s1.Should().BeSameAs(s2);
+        }
+    }
+}
